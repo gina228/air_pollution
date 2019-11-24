d56f233d35db5307876ef1f0c20e35c07e547292
Gina Graci <ginagraci92@gmail.com>
2019-11-24 18:11:42 +0000

    added attr accessor and city properties to city file

--- lib/air_pollution/city.rb
+++ lib/air_pollution/city.rb
@@ -1,5 +1,7 @@
 class AirPollution::City
   
+  attr_accessor :name, :pollution
+  
   def self.today
      puts <<-DOC
       1. New York, New York
@@ -7,6 +9,24 @@ class AirPollution::City
       3. Cleveland, Ohio
       4. Honolulu, Hawaii
     DOC
+    
+    city_1 = self.new
+    city_1.name = "New York, New York"
+    city_1.pollution = 
+    
+    city_2 = self.new
+    city_2.name = "Lost Angeles, California"
+    city_2.pollution =
+    
+    city_3 = self.new
+    city_3.name = "Cleveland, Ohio"
+    city_3.pollution =
+    
+    city_4 = self.new
+    city_4.name = "Honolulu, Hawaii"
+    city_4.pollution =
+    
+    [city_1, city_2, city_3, city_4]
   end 
   
 end 
