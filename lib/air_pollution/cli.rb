18d2f6e3e05dec13db100024a5a47ffcd78a2b75
Gina Graci <ginagraci92@gmail.com>
2019-11-24 18:16:01 +0000

    added if, elsif, and else statement into cli file

--- lib/air_pollution/cli.rb
+++ lib/air_pollution/cli.rb
@@ -17,19 +17,13 @@ class AirPollution::CLI
     input = nil
     while input != "exit"
      puts "Enter the number of the city you would like to view air pollution data on. Type list to see the list of cities. Type exit to exit."
-      input = gets.strip.downcase 
-      case input
-      when "1"
-        puts "NY"
-      when "2"
-        puts "CA"
-      when "3"
-        puts "OH"
-      when "4"
-        puts "HI"
-      when "list"
-        list_cities
-      else
+      input = gets.strip.downcase
+      
+        if input.to_i > 0
+          puts @cities[input.to_i-1]
+        elsif input == "list"
+          list_cities
+        else
         puts "Not a valid entry."
       end 
     end
